4544060198193770998f277bb9d41cc228c00a66
Ingo Kuba <ingo.kuba@yahoo.de>
2024-09-15 13:01:16 +0200

    Deploy to GitHub pages (#2)
    Co-authored-by: ingokuba <ingokuba@github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://ingokuba.github.io/",
+  base: "gitgram",
   trailingSlash: "always",
   integrations: [
     tailwind(),
